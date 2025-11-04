d19d439738e114231c621006caa3a7af9b6bc8c3
Pamela <pamela.jesus.lima@gmail.com>
2025-11-04 13:54:40 -0300

    whatsapp message updated

--- src/components/WhatsappButton/WhatsappButton.tsx
+++ src/components/WhatsappButton/WhatsappButton.tsx
@@ -4,7 +4,7 @@ import styles from "./WhatsappButton.module.css";
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/5511984097584?text=Olá,%20vi%20seu%20anúncio%20e%20gostaria%20de%20mais%20informações!"
+      href="https://wa.me/5511984097584?text=Olá,%20vi%20seu%20portfólio%20e%20gostaria%20de%20mais%20informações!"
       target="_blank"
       rel="noopener noreferrer"
       className={styles.whatsapp_button}
