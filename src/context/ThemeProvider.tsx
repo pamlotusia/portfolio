5284edeec68950ef0d3cc90673b6a5ccfebe8572
Pamela <pamela.jesus.lima@gmail.com>
2025-11-04 12:32:34 -0300

    correcting ts bugs

---
+++ src/context/ThemeProvider.tsx
@@ -0,0 +1,24 @@
+import { ThemeContext } from "./ThemeContext";
+import { useState, useEffect, type ReactNode } from "react";
+import  "../styles/themes/light.module.css";
+import "../styles/themes/dark.module.css";
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState<"light" | "dark">("dark");
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  useEffect(() => {
+    document.body.classList.remove("light-theme", "dark-theme");
+    document.body.classList.add(
+      theme === "light" ? "light-theme" : "dark-theme"
+    );
+  }, [theme]);
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
